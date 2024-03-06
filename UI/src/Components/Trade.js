4e266c480d79b48c0c1ad6b7644942d036219a6c
Sierra-Lima <114910174+Sierra-Lima@users.noreply.github.com>
2024-03-06 21:44:15 +0800

    UI

---
+++ UI/src/Components/Trade.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+
+function Trade(){
+    return(
+        <div>
+            <h1>Not sure how we want to build this page</h1>
+        </div>
+    )
+}
+
+export default Trade;
