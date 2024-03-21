1bdef7b960c25f873f96d72ceeb411ca46455e05
nmywrld <drago-tan@hotmail.com>
2024-03-21 22:04:09 +0800

    update: docker compose all

--- tag_images.sh
+++ tag_images.sh
@@ -4,16 +4,22 @@
 USERNAME="nmywrld"
 
 # Define the names of your Docker images
-IMAGES=("userservices-user_service" "userservices-user_db" "sentimentservice-sentiment_app" "sentimentservice-sentiment_service" "rabbitmq-rabbitmq" "notificationservice-notification_manager" "kongservice-kong" "notificationservice-telebot" "notificationservice-watchlist")
+IMAGES=("userservices-user_service" "userservices-user_db" "sentimentservice-sentiment_app" "sentimentservice-sentiment_service" "rabbitmq-rabbitmq" "notificationservice-notification_manager" "kongservice-kong" "notificationservice-telebot" "notificationservice-watchlist" "notificationservice-watchlist_db" "sentimentservice-sentiment_db")
 
 # Define the tags you want to assign to the images
 TAGS=("tag1" "tag2" "tag3" "tag4" "tag5" "tag6" "tag7" "tag8" "tag9" "tag10")
 
 # Loop through each image and tag combination
 for ((i=0; i<${#IMAGES[@]}; i++)); do
-    docker tag "${IMAGES[$i]}" "$USERNAME/${IMAGES[$i]}:latest"
 
+    ####### TAG for ARM #######
+    # docker tag "${IMAGES[$i]}" "$USERNAME/arm/${IMAGES[$i]}:latest"
+    # docker push "$USERNAME/arm/${IMAGES[$i]}:latest"
+
+    ####### TAG for x86 #######
+    docker tag "${IMAGES[$i]}" "$USERNAME/${IMAGES[$i]}:latest"
     docker push "$USERNAME/${IMAGES[$i]}:latest"
+
 
     # docker push "$IMAGES[$i]:latest"
 done
