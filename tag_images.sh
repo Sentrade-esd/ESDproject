dc7fd0069bec4b54b3c1d3e979c8d603b8b2c2b6
nmywrld <111645407+nmywrld@users.noreply.github.com>
2024-03-21 14:20:17 +0800

    update main compose

---
+++ tag_images.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Define your Docker Hub username
+USERNAME="nmywrld"
+
+# Define the names of your Docker images
+IMAGES=("userservices-user_service" "userservices-user_db" "sentimentservice-sentiment_app" "sentimentservice-sentiment_service" "rabbitmq-rabbitmq" "notificationservice-notification_manager" "kongservice-kong" "notificationservice-telebot" "notificationservice-watchlist")
+
+# Define the tags you want to assign to the images
+TAGS=("tag1" "tag2" "tag3" "tag4" "tag5" "tag6" "tag7" "tag8" "tag9" "tag10")
+
+# Loop through each image and tag combination
+for ((i=0; i<${#IMAGES[@]}; i++)); do
+    # docker tag "${IMAGES[$i]}" "$USERNAME/${IMAGES[$i]}:${IMAGES[$i]}"
+
+    # docker push "$USERNAME/${IMAGES[$i]}:${IMAGES[$i]}"
+
+    docker push "$IMAGES[$i]:latest"
+done
