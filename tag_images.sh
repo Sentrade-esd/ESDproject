57042adc1fab16c81a0fbc970a2797b89e2df354
nmywrld <drago-tan@hotmail.com>
2024-03-21 22:57:32 +0800

    Update tag_images.sh

--- tag_images.sh
+++ tag_images.sh
@@ -6,8 +6,6 @@ USERNAME="nmywrld"
 # Define the names of your Docker images
 IMAGES=("userservices-user_service" "userservices-user_db" "sentimentservice-sentiment_app" "sentimentservice-sentiment_service" "rabbitmq-rabbitmq" "notificationservice-notification_manager" "kongservice-kong" "notificationservice-telebot" "notificationservice-watchlist" "notificationservice-watchlist_db" "sentimentservice-sentiment_db")
 
-# Define the tags you want to assign to the images
-TAGS=("tag1" "tag2" "tag3" "tag4" "tag5" "tag6" "tag7" "tag8" "tag9" "tag10")
 
 # Loop through each image and tag combination
 for ((i=0; i<${#IMAGES[@]}; i++)); do
