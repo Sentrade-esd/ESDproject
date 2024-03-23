6140330f37f8426903fb7b9b28bca29d6c52b815
nmywrld <111645407+nmywrld@users.noreply.github.com>
2024-03-23 19:12:23 +0800

    update: dockerise transactions

---
+++ userServices/Transactions/init.sql
@@ -0,0 +1,18 @@
+-- Create the database if it doesn't exist
+CREATE DATABASE transaction_data;
+
+-- Connect to the newly created database
+\c transaction_data;
+
+-- Create a table for your Transaction model
+CREATE TABLE IF NOT EXISTS "transaction" (
+    TransactionID SERIAL PRIMARY KEY,
+    UserID INTEGER NOT NULL,
+    Email VARCHAR(255) NOT NULL,
+    Company VARCHAR(255) NOT NULL,
+    DateTimestamp TIMESTAMP NOT NULL,
+    BuyAmount NUMERIC(10, 2) NOT NULL,
+    SellAmount NUMERIC(10, 2),
+    StopLossSentimentThreshold NUMERIC(10, 2) NOT NULL,
+    TotalAccountValue NUMERIC(10, 2) NOT NULL
+);
