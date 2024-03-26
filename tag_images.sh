c987da70f8655d245f802d91180f69b0d0fd53a9
nmywrld <111645407+nmywrld@users.noreply.github.com>
2024-03-26 17:34:16 +0800

    Update tag_images.sh

--- tag_images.sh
+++ tag_images.sh
@@ -4,17 +4,12 @@
 USERNAME="nmywrld"
 
 # Define the names of your Docker images
-IMAGES=("userservices-user_service" "userservices-user_db" "sentimentservice-sentiment_app" "sentimentservice-sentiment_service" "rabbitmq-rabbitmq" "notificationservice-notification_manager" "kongservice-kong" "notificationservice-telebot" "notificationservice-watchlist" "notificationservice-watchlist_db" "sentimentservice-sentiment_db")
+IMAGES=("esdproject-telebot" "esdproject-watchlist" "esdproject-follow_trades" "esdproject-comments_service" "esdproject-senator_filings" "esdproject-scraper" "esdproject-sentiment_app" "esdproject-sentiment_service" "esdproject-user_db" "esdproject-transactions_db" "esdproject-rabbitmq" "esdproject-user_service" "esdproject-transactions" "esdproject-notification_manager" "esdproject-sentiment_db" "esdproject-comments_db" "esdproject-scraper_db" "esdproject-watchlist_db" "esdproject-kong")
 
 
 # Loop through each image and tag combination
 for ((i=0; i<${#IMAGES[@]}; i++)); do
 
-    ####### TAG for ARM #######
-    # docker tag "${IMAGES[$i]}" "$USERNAME/arm/${IMAGES[$i]}:latest"
-    # docker push "$USERNAME/arm/${IMAGES[$i]}:latest"
-
-    ####### TAG for x86 #######
     docker tag "${IMAGES[$i]}" "$USERNAME/${IMAGES[$i]}:latest"
     docker push "$USERNAME/${IMAGES[$i]}:latest"
 
