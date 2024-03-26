279cc212bd865511b35c53179efbc252eb5ee46c
ReFlecT21 <kumaraguru818@gmail.com>
2024-03-26 16:42:42 +0800

    fix: changed tele notif

--- notificationService/manage_notifications/index.js
+++ notificationService/manage_notifications/index.js
@@ -15,37 +15,37 @@ const amqp_url = process.env.AMQP_SERVER || "amqp://localhost:5672";
 const watchlist_url = process.env.WATCHLIST_URL || "http://localhost:3000/";
 const telebut_url = process.env.TELEBOT_URL || "http://localhost:3002/";
 
-let connection= null;
+let connection = null;
 let channel = null;
 
 app.use(express.json());
 
 async function start_amqp() {
   return new Promise((resolve, reject) => {
-      const start = async () => {
-          try {
-              connection = await amqplib.connect(amqp_url);
-              channel = await connection.createChannel();
+    const start = async () => {
+      try {
+        connection = await amqplib.connect(amqp_url);
+        channel = await connection.createChannel();
 
-              connection.on('error', async (err) => {
-                  if (err.message !== 'Connection closing') {
-                      console.error('[AMQP] conn error', err.message);
-                  }
-              });
-
-              connection.on('close', () => {
-                  console.error('[AMQP] reconnecting');
-                  return setTimeout(start, 10000);
-              });
-
-              console.log('[AMQP] connected');
-              resolve();
-          } catch (err) {
-              console.error('[AMQP] could not connect', err.message);
-              return setTimeout(start, 10000);
+        connection.on("error", async (err) => {
+          if (err.message !== "Connection closing") {
+            console.error("[AMQP] conn error", err.message);
           }
-      };
-      start();
+        });
+
+        connection.on("close", () => {
+          console.error("[AMQP] reconnecting");
+          return setTimeout(start, 10000);
+        });
+
+        console.log("[AMQP] connected");
+        resolve();
+      } catch (err) {
+        console.error("[AMQP] could not connect", err.message);
+        return setTimeout(start, 10000);
+      }
+    };
+    start();
   });
 }
 
@@ -54,17 +54,24 @@ async function start_amqp() {
   // await channel.assertQueue(queue, { durable: true });
 
   channel.consume(queue, async (message) => {
-
     // for each message, parse the message and send to telegram)
     const parsedMessage = JSON.parse(message.content.toString());
 
-    for (let i = 0; i < parsedMessage.length; i++){
+    for (let i = 0; i < parsedMessage.length; i++) {
       const content = parsedMessage[i];
       console.log(content);
       const company = content.search;
       const change = content.change;
-
-      console.log(`Received message for company ${company} with change ${change}`);
+      let changeType = "  ";
+      if (Number(change) < 0) {
+        changeType = "decreased";
+      } else {
+        changeType = "increased";
+      }
+      change = Math.abs(change) * 100;
+      console.log(
+        `Received message for company ${company} with change ${change}`
+      );
 
       try {
         const response = await axios.get(
@@ -74,20 +81,18 @@ async function start_amqp() {
 
         // Send message to Telegram bot
         const telegramIds = response.data; // Replace this with the actual array of Telegram IDs
-        const teleMessage = `The company ${company} has a change of ${change}.`; // Replace this with the actual message
+        const teleMessage = `The company: ${company} has a ${changeType} by ${change} %.`; // Replace this with the actual message
 
         await axios.post(`${telebut_url}teleBot/send_message`, {
           telegramIds,
           teleMessage,
         });
-        
       } catch (error) {
         console.error(`Error getting watchlist for company ${company}:`, error);
       }
-      
     }
     channel.ack(message);
-    
+
     // console.log(content);
     // const company = content.search;
     // const change = content.change;
@@ -113,9 +118,7 @@ async function start_amqp() {
     // } catch (error) {
     //   console.error(`Error getting watchlist for company ${company}:`, error);
     // }
-
   });
-
 })();
 
 app.listen(PORT, () => {
