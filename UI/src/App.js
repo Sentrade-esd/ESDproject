5b40c42cfff37b7847060b697005348c6a5f4d61
ReFlecT21 <kumaraguru818@gmail.com>
2024-03-28 15:14:26 +0800

    chore: changed case

--- UI/src/App.js
+++ UI/src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import Navigation from './Components/Navigation.js'
-import NavBar from './Components/NavBar.js'
-import HomeBody from './Pages/HomeBody.js'
-import Home from './Pages/Home.js'
-import { BrowserRouter, Routes, Route, useLocation} from "react-router-dom";
-import Search from './Pages/Search.js';
-import Trade from './Pages/Trade.js';
-import TradeHistory from './Pages/TradeHistory.js';
-import React, {useState, useEffect} from 'react';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Navigation from "./Components/Navigation.js";
+import NavBar from "./Components/NavBar.js";
+import HomeBody from "./Pages/HomeBody.js";
+import Home from "./Pages/Home.js";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Search from "./Pages/Search.js";
+import Trade from "./Pages/Trade.js";
+import TradeHistory from "./Pages/TradeHistory.js";
+import React, { useState, useEffect } from "react";
 
 function Main() {
   const location = useLocation();
 
   return (
     <div className="App">
-      {location.pathname !== '/landing' && <NavBar transparent={false} />}
+      {location.pathname !== "/landing" && <NavBar transparent={false} />}
       <Routes>
         <Route path="/landing" element={<Home />} />
         <Route path="/" element={<HomeBody />} />
@@ -36,6 +35,5 @@ function App() {
     </BrowserRouter>
   );
 }
-
 
 export default App;
