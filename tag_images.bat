9e24cf696de3e47454325f566d23514950fec65c
nmywrld <111645407+nmywrld@users.noreply.github.com>
2024-04-04 11:59:21 +0800

    update tag

--- tag_images.bat
+++ tag_images.bat
@@ -1,6 +1,6 @@
 @echo off
 set USERNAME=chernise
-set IMAGES= esdproject-telebot esdproject-watchlist esdproject-follow_trades esdproject-comments_service esdproject-senator_filings esdproject-scraper esdproject-sentiment_app esdproject-user_db esdproject-transactions_db esdproject-rabbitmq esdproject-user_service esdproject-transactions esdproject-notification_manager esdproject-sentiment_db esdproject-comments_db esdproject-scraper_db esdproject-watchlist_db esdproject-kong esdproject-app esdproject-grafana esdproject-prometheus
+set IMAGES= esdproject-telebot esdproject-watchlist esdproject-follow_trades esdproject-comments_service esdproject-sentiment_service esdproject-senator_filings esdproject-scraper esdproject-sentiment_app esdproject-user_db esdproject-transactions_db esdproject-rabbitmq esdproject-user_service esdproject-transactions esdproject-notification_manager esdproject-sentiment_db esdproject-comments_db esdproject-scraper_db esdproject-watchlist_db esdproject-kong esdproject-app esdproject-grafana esdproject-prometheus
 @REM set IMAGES=esdproject-telebot esdproject-app esdproject-sentiment_service
 
 
--- tag_images.sh
+++ tag_images.sh
@@ -4,7 +4,7 @@
 USERNAME="chernise"
 
 # Define the names of your Docker images
-IMAGES=("esdproject-telebot" "esdproject-watchlist" "esdproject-follow_trades" "esdproject-comments_service" "esdproject-senator_filings" "esdproject-scraper" "esdproject-sentiment_app" "esdproject-sentiment_service" "esdproject-user_db" "esdproject-transactions_db" "esdproject-rabbitmq" "esdproject-user_service" "esdproject-transactions" "esdproject-notification_manager" "esdproject-sentiment_db" "esdproject-comments_db" "esdproject-scraper_db" "esdproject-watchlist_db" "esdproject-kong" "esdproject-app" "esdproject-grafana" "esdproject-prometheus")
+IMAGES=("esdproject-telebot" "esdproject-watchlist" "esdproject-follow_trades" "esdproject-comments_service" "esdproject-sentiment_service" "esdproject-senator_filings" "esdproject-scraper" "esdproject-sentiment_app" "esdproject-user_db" "esdproject-transactions_db" "esdproject-rabbitmq" "esdproject-user_service" "esdproject-transactions" "esdproject-notification_manager" "esdproject-sentiment_db" "esdproject-comments_db" "esdproject-scraper_db" "esdproject-watchlist_db" "esdproject-kong" "esdproject-app" "esdproject-grafana" "esdproject-prometheus")
 
 
 # Loop through each image and tag combination
