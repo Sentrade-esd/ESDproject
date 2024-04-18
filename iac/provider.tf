cd83873bc3c1601f73960fd0a561756a00252e8a
nmywrld <111645407+nmywrld@users.noreply.github.com>
2024-04-18 18:45:49 +0800

    update: GKE provider

--- iac/provider.tf
+++ iac/provider.tf
@@ -3,3 +3,31 @@ provider "google" {
   region  = "asia-east1"  # Change to your desired region
 }
 
+resource "google_container_cluster" "my_cluster" {
+  name     = "my-cluster"
+  location = "us-central1"  # Change to your desired region
+
+  # Node pool configuration
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  # Add additional node pool configurations if needed
+}
+
+resource "google_container_node_pool" "primary_preemptible_nodes" {
+  name       = "my-node-pool"
+  cluster    = google_container_cluster.primary.id
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    service_account = google_service_account.default.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
+
