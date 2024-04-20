7af53d74bdd5130f83658007926be54a534a88ae
nmywrld <drago-tan@hotmail.com>
2024-04-20 20:50:07 +0800

    update: change deploy script

--- iac/main.tf
+++ iac/main.tf
@@ -46,14 +46,22 @@ resource "google_container_node_pool" "primary_nodes" {
 }
 
 resource "null_resource" "deploy_app" {
+    triggers = {
+    always_run = "${timestamp()}"
+  }
+
   provisioner "local-exec" {
     command = <<EOF
       gcloud container clusters get-credentials ${google_container_cluster.primary.name} --region ${var.region}
+      for file in $(echo "${join(" ", var.files)}") ; do
+        kubectl delete -f ./deployments/$file --ignore-not-found
+      done
       for file in $(echo "${join(" ", var.files)}") ; do
         kubectl apply -f ./deployments/$file
       done
     EOF
   }
+
   depends_on = [
     google_container_cluster.primary,
     google_container_node_pool.primary_nodes
