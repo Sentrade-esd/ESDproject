40b37b38a3497d0261f49d7976dc53b773b6deab
nmywrld <drago-tan@hotmail.com>
2024-04-20 23:58:36 +0800

    update: node size

--- iac/main.tf
+++ iac/main.tf
@@ -31,7 +31,7 @@ resource "google_container_cluster" "primary" {
 resource "google_container_node_pool" "primary_nodes" {
   name       = "esd-node-pool"
   cluster    = google_container_cluster.primary.id
-  node_count = 1
+  node_count = 3
 
   node_config {
     # machine_type = "e2-medium"
