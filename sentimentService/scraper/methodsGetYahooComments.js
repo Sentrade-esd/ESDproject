1a3a10c3ccf91f2d0c22d51eb9721f447cf9308e
yixuan0922 <yixuan0922@hotmail.com>
2024-04-24 02:42:12 +0800

    Fix: getYahooComments works

--- sentimentService/scraper/methodsGetYahooComments.js
+++ sentimentService/scraper/methodsGetYahooComments.js
@@ -1,6 +1,5 @@
 const { parentPort } = require("worker_threads");
 let puppeteer = require("puppeteer");
-
 require("dotenv").config();
 
 require("./scraperDBclient.js");
@@ -12,76 +11,107 @@ const sleep = (milliseconds) => {
 };
 
 async function performScraping(query) {
-
   let browser = await puppeteer.launch({
-    headless: "new",
+    headless: 'new',
     // executablePath: "/usr/bin/chromium",
     args: ["--no-sandbox"],
   });
   let page = await browser.newPage();
 
-  let url = `https://sg.finance.yahoo.com/quote/${query}/community`;
+  let url = `https://finance.yahoo.com/quote/${query}/community`;
   // await page.goto(url);
-  await page.goto(url, { waitUntil: "networkidle2" });
 
-  await sleep(30000);
-  const cookies = await page.cookies();
-  const cookieHeader = cookies.map(cookie => `${cookie.name}=${cookie.value}`).join(`; `);
-  const xAccessToken = await page.evaluate(() => {
-    return sessionStorage.getItem("x-access-token");
+  let headers = {};
+  let postData = {};
+
+  let requestIntercepted = false;
+
+  // Prepare to capture the request
+  page.on('request', async interceptedRequest => {
+    if (interceptedRequest.url().includes('api-2-0.spot.im/v1.0.0/conversation/read') && interceptedRequest.method() === 'POST') {
+      headers = interceptedRequest.headers();
+      postData = interceptedRequest.postData();
+
+      console.log("Captured Headers:", headers);
+      console.log("Captured Post Data:", postData);
+      console.log("Captured URL:", interceptedRequest.url());
+
+      requestIntercepted = true;
+
+    }
   });
 
-  console.log("CookieHeader:", cookieHeader, "xAccessToken:", xAccessToken);
+  // await page.goto(url, { waitUntil: "networkidle2" });
+  await page.goto(url);
+// Scroll to the middle of the page
+  await page.evaluate(() => {
+    window.scrollTo(0, document.body.scrollHeight / 2);
+  });
+
+
+  console.log("Waiting for request to be intercepted:", requestIntercepted);
+  // Wait for the request to be intercepted
+  while (!requestIntercepted) {
+    await sleep(1000);
+    console.log("Sleeping 1s")
+  }
+
   await browser.close();
 
   var options = {
     'method': 'POST',
     'url': 'https://api-2-0.spot.im/v1.0.0/conversation/read',
     'headers': {
-      'accept': 'application/json',
-      'accept-language': 'en-US,en;q=0.9',
-      'content-type': 'application/json',
-      'cookie': cookieHeader,
-      'dnt': '1',
-      'origin': 'https://openweb.jac.yahoosandbox.com',
-      'referer': 'https://openweb.jac.yahoosandbox.com/2.0.0/safeframe.html',
-      'sec-ch-ua': '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
-      'sec-ch-ua-mobile': '?0',
-      'sec-ch-ua-platform': '"macOS"',
-      'sec-fetch-dest': 'empty',
-      'sec-fetch-mode': 'cors',
-      'sec-fetch-site': 'cross-site',
-      'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
-      'x-access-token': xAccessToken,
-      'x-post-id': 'finmb$24937',
-      'x-spot-id': 'sp_Rba9aFpG',
-      'x-spotim-device-uuid': '8bb4f001-04d9-4787-b6ef-5ab7726a4432',
-      'x-spotim-page-view-id': '4f23009e-0892-422d-ae32-cfb50c875235'
+      'authority': 'api-2-0.spot.im', //accurate
+      'accept': 'application/json', //accurate
+      'accept-language': 'en-US,en;q=0.9', //accurate
+      'content-type': 'application/json', // accurate
+      'Cookie': `device_uuid=${headers["x-spotim-device-uuid"]}; access_token=${headers["x-access-token"]}; device_uuid=${headers["x-spotim-device-uuid"]}`,
+      // 'cookie': `device_uuid=${headers["x-spotim-device-uuid"]}`, // accurate
+      'origin': 'https://openweb.jac.yahoosandbox.com', // accurate
+      'referer': 'https://openweb.jac.yahoosandbox.com/2.0.0/safeframe.html',  // accurate
+      'sec-ch-ua': '"Chromium";v="121", "Not A(Brand";v="99"',
+      'sec-ch-ua-mobile': '?0', // accurate
+      'sec-ch-ua-platform': '"macOS"', // accurate
+      'sec-fetch-dest': 'empty', // accurate
+      'sec-fetch-mode': 'cors', // accurate
+      'sec-fetch-site': 'cross-site', // accurate
+      'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36', // accurate
+      'x-access-token': headers["x-access-token"],  // accurate
+      'x-post-id': headers["x-post-id"],  // accurate
+      'x-spot-id': headers["x-spot-id"],  // accurate
+      'x-spotim-device-uuid': headers["x-spotim-device-uuid"],  // accurate
+      'x-spotim-page-view-id': headers["x-spotim-page-view-id"],  // accurate
     },
-    body: JSON.stringify({
-      "sort_by": "newest",
+    data:{
+      "sort_by": "best",
       "offset": 0,
-      "count": 10,
+      "count": 30,
       "depth": 2,
-      "child_count": 2
-    })
+      "child_count": 2,
+      "conversation_id": `${headers["x-spot-id"]}${headers["x-post-id"]}`,
+    }
   };
-  
+
+  // let response = await request(options);
   let response = await axios(options);
-  console.log(response.data);
+  let parsedResponse = response.data;
+  let comments = parsedResponse["conversation"].comments;
+  let data = [];
+
+  comments.map((comment) => {
+    data.push(comment.content[0].text);
+  });
+
+  // let comments = parsedResponse["conversation"]['comments']
+  // let comments = JSON.parse(response).conversation.comments;
+  // let commentsBody = [];
+  // comments.map((comment)=> {
+  //   console.log("Comment:", comment.content.text);
+  //   commentsBody.push(comment.content.text);
+  // })
 
 
-  let data = {};
-  
-  // data.marketCap = await page.evaluate((marketCapSelector) => {
-  //   return document.querySelector(marketCapSelector).innerText;
-  // }, marketCapSelector);
-  // data.avgVolume = await page.evaluate((avgVolumeSelector) => {
-  //   return document.querySelector(avgVolumeSelector).innerText;
-  // }, avgVolumeSelector);
-  // data.previousClose = await page.evaluate((previousCloseSelector) => {
-  //   return document.querySelector(previousCloseSelector).innerText;
-  // }, previousCloseSelector);
 
   return data;
 
@@ -98,9 +128,16 @@ parentPort.on("message", async (message) => {
     // res.send(response);
 
     let response = await performScraping(ticker);
-    response.ticker = ticker;
-    // console.log('response', response);
-    parentPort.postMessage(response);
+    if (response) {
+        response.ticker = ticker;
+        parentPort.postMessage(response);
+    } else {
+        parentPort.postMessage({ error: "Failed to get response" });
+    }
+    // let response = await performScraping(ticker);
+    // response.ticker = ticker;
+    // // console.log('response', response);
+    // parentPort.postMessage(response);
   } catch (error) {
     // Handle errors
     parentPort.postMessage({ error: error.message });
