aaef68b70057bec11e326d28e5423a168ec5dd2b
nmywrld <drago-tan@hotmail.com>
2024-04-28 20:59:53 +0800

    Update tag_images.sh

--- tag_images.sh
+++ tag_images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define your Docker Hub username
-USERNAME="nmywrld"
+USERNAME="chernise"
 
 # Define the names of your Docker images
 IMAGES=("esdproject-telebot" "esdproject-watchlist" "esdproject-follow_trades" "esdproject-comments_service" "esdproject-sentiment_service" "esdproject-senator_filings" "esdproject-scraper" "esdproject-sentiment_app" "esdproject-user_db" "esdproject-transactions_db" "esdproject-rabbitmq" "esdproject-user_service" "esdproject-transactions" "esdproject-notification_manager" "esdproject-sentiment_db" "esdproject-comments_db" "esdproject-scraper_db" "esdproject-watchlist_db" "esdproject-kong" "esdproject-app" "esdproject-grafana" "esdproject-prometheus")
@@ -10,7 +10,7 @@ IMAGES=("esdproject-telebot" "esdproject-watchlist" "esdproject-follow_trades" "
 # Loop through each image and tag combination
 for ((i=0; i<${#IMAGES[@]}; i++)); do
     
-    sudo docker tag "${IMAGES[$i]}" "$USERNAME/${IMAGES[$i]}:latest"
-    sudo docker push "$USERNAME/${IMAGES[$i]}:latest"
+    docker tag "${IMAGES[$i]}" "$USERNAME/${IMAGES[$i]}:latest"
+    docker push "$USERNAME/${IMAGES[$i]}:latest"
     # docker push "$IMAGES[$i]:latest"
 done
